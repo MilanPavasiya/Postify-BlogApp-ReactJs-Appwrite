c6cf719cd8bb849f5d22382ee2795dfc25caad83
MilanPavasiya <milpavasiya@gmail.com>
2025-11-09 08:26:52 -0500

    feat: add Appwrite auth service

---
+++ Postify/src/appwrite/auth.js
@@ -0,0 +1,62 @@
+import config from '../config/config.js';
+import { Client, Account, ID } from 'appwrite';
+
+export class AuthService {
+	client = new Client();
+	account;
+
+	constructor() {
+		this.client
+			.setEndpoint(config.appwrite.url)
+			.setProject(config.appwrite.projectId);
+		this.account = new Account(this.client);
+	}
+
+	async createAccount({ email, password, name }) {
+		try {
+			const userAccount = await this.account.create(
+				(ID.unique(), email, password, name)
+			);
+
+			if (userAccount) {
+				return this.login({ email, password });
+			} else {
+				return userAccount;
+			}
+		} catch (error) {
+			console.log('Error in createAccount', error);
+		}
+	}
+
+	async login({ email, password }) {
+		try {
+			return await this.account.createEmailPasswordSession(email, password);
+		} catch (error) {
+			console.log('Error in login', error);
+		}
+	}
+
+	async getCurrentUser() {
+		try {
+			return await this.account.get();
+		} catch (error) {
+			console.log('Error in getCurrentUser', error);
+		}
+
+		return null;
+	}
+
+	async logout() {
+		try {
+			return await this.account.deleteSessions();
+		} catch (error) {
+			console.log('Error in logout', error);
+		}
+
+		return null;
+	}
+}
+
+const authService = new AuthService();
+
+export default authService;
