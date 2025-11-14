d962223d9737bb7d9727785d5f16af5b27d52969
MilanPavasiya <milpavasiya@gmail.com>
2025-11-14 12:19:27 -0500

    index file for all components export

---
+++ Postify/src/components/index.js
@@ -0,0 +1,31 @@
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import Container from './Container/Container';
+import Logo from './Logo';
+import LogoutBtn from './Header/LogoutBtn';
+import Button from './Button';
+import Input from './Input';
+import RTE from './RTE';
+import Signup from './Signup';
+import PostForm from './PostForm/PostForm';
+import PostCard from './PostCard';
+import AuthLayout from './AuthLayout';
+import Login from './Login';
+import Select from './Select';
+
+export {
+	Header,
+	Footer,
+	Container,
+	Logo,
+	LogoutBtn,
+	Button,
+	Input,
+	RTE,
+	Signup,
+	PostForm,
+	PostCard,
+	AuthLayout,
+	Login,
+	Select,
+};
