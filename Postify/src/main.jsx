560cb6ddb5ec08f4400f5d496d34e846acf24caf
MilanPavasiya <milpavasiya@gmail.com>
2025-11-15 12:03:29 -0500

    setup routing with main pages and auth layout

--- Postify/src/main.jsx
+++ Postify/src/main.jsx
@@ -4,11 +4,76 @@ import './index.css';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from 'react-router-dom';
+import { AuthLayout } from './components/index';
+import {
+	AddPost,
+	AllPosts,
+	EditPost,
+	Home,
+	Login,
+	Post,
+	Signup,
+} from './Pages';
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<App />}>
+			<Route path='/' element={<Home />} />
+			<Route
+				path='/login'
+				element={
+					<AuthLayout authentication={false}>
+						<Login />
+					</AuthLayout>
+				}
+			/>
+			<Route
+				path='/signup'
+				element={
+					<AuthLayout authentication={false}>
+						<Signup />
+					</AuthLayout>
+				}
+			/>
+			<Route
+				path='/all-posts'
+				element={
+					<AuthLayout authentication>
+						<AllPosts />
+					</AuthLayout>
+				}
+			/>
+			<Route
+				path='/add-post'
+				element={
+					<AuthLayout authentication>
+						<AddPost />
+					</AuthLayout>
+				}
+			/>
+			<Route
+				path='/edit-post/:slug'
+				element={
+					<AuthLayout authentication>
+						<EditPost />
+					</AuthLayout>
+				}
+			/>
+			<Route path='/post/:slug' element={<Post />} />
+		</Route>
+	)
+);
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
 		<Provider store={store}>
-			<App />
+			<RouterProvider router={router} />
 		</Provider>
 	</StrictMode>
 );
